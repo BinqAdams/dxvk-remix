78b86a897bd7cc1b468fa9e6d4430b454c8b3fba
Binq Adams <20955335+BinqAdams@users.noreply.github.com>
2026-08-13 02:15:41 +0930

    Revert "rtx: DLFG present-metering synchronization fix for multi-frame generation"
    This reverts commit ac26d19b379fa9c6d07e81bcab370169a587437e.

--- src/dxvk/dxvk_device_info.h
+++ src/dxvk/dxvk_device_info.h
@@ -58,10 +58,6 @@ namespace dxvk {
     VkPhysicalDeviceDiagnosticsConfigFeaturesNV               nvDeviceDiagnosticsConfig;
     VkPhysicalDeviceBufferDeviceAddressFeaturesKHR            khrBufferDeviceAddress;
 
-    // NV-DXVK start: DLFG present metering
-    VkPhysicalDevicePresentMeteringFeaturesNV                  nvPresentMetering;
-    // NV-DXVK end
-
     // NV-DXVK start: opacity micromap
     VkPhysicalDeviceSynchronization2FeaturesKHR               khrSynchronization2;
     // NV-DXVK end
